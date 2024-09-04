e7ea8157739c3b7b4ae2177f5d2aeb4b8abaae28
shubham@workstation <shubhamanitasingh@gmail.com>
2024-09-04 19:01:59 +0530

    extension.js: fetch list of theme script and its about section

--- extension.js
+++ extension.js
@@ -1,1 +1,47 @@
-fzf - m--preview = "head -n 20 {}" --preview - window="right:20%,wrap"
+//fzf - m--preview = "head -n 20 {}" --preview - window="right:20%,wrap"
+import { curlRequest } from '../justjs/src/curl.js'
+
+async function getThemeExtensionScripts() {
+  const owner = "5hubham5ingh";
+  const repo = "WallWiz";
+  const path = "themeExtensionScripts";
+  const url = `https://api.github.com/repos/${owner}/${repo}/contents/${path}`;
+
+  const handleError = (error) => {
+    print("Failed to fetch list of theme extension scripts.", error)
+  }
+  const handleSuccess = res => res.filter(script => script.type === 'file');
+
+  return await curlRequest(url, {
+    parseJson: true
+  })
+    .then(handleSuccess)
+    .then(async scripts => {
+      const promises = [];
+      for (const script of scripts) {
+        const getScriptPromise = getScriptHead(script.download_url)
+          .then(head => ({
+            name: script.name,
+            about: head,
+            download_url: script.download_url
+          }));
+        promises.push(getScriptPromise);
+      }
+
+      return await Promise.all(promises);
+    })
+    .catch(handleError)
+}
+
+async function getScriptHead(url) {
+  return curlRequest(url, {
+    headers: {
+      'Range': 'bytes=0-499'
+    }
+  }).catch(e => print('Failed to fetch script head', e))
+}
+
+
+await getThemeExtensionScripts().then(res => print(JSON.stringify(res)));
+
+
