3d4e59c6f2d406c3252bb5c80b45a5b56aeb042f
ss <ss.dev.me@gmail.con>
2024-10-23 22:24:35 +0530

    Update: Build.sh; Added: ensureDir function definition in utils;

--- build.sh
+++ build.sh
@@ -1,26 +1,28 @@
+#!/bin/env bash
+
 # Build script for WallWiz
 
 ## Fetch the QuickJS source code, then build and install the compiler and interpreter in the system.
-git clone --depth 1 https://github.com/bellard/quickjs.git &&
-cd quickjs &&
-make &&
-sudo make install &&
-cd .. &&
+   git clone --depth 1 https://github.com/bellard/quickjs.git &&
+   cd quickjs &&
+   make &&
+   sudo make install &&
+   cd .. &&
 
 ## Fetch the required library.
-curl -L -o out.zip https://github.com/ctn-malone/qjs-ext-lib/archive/refs/tags/0.12.4.zip &&
-unzip out.zip &&
-mv qjs-ext-lib-0.12.4 qjs-ext-lib &&
-rm out.zip &&
+   curl -L -o out.zip https://github.com/ctn-malone/qjs-ext-lib/archive/refs/tags/0.12.4.zip &&
+   unzip out.zip &&
+   mv qjs-ext-lib-0.12.4 qjs-ext-lib &&
+   rm out.zip &&
 
 ## Fetch helper scripts
-git clone --depth 1 https://github.com/5hubham5ingh/justjs.git &&
+   git clone --depth 1 https://github.com/5hubham5ingh/justjs.git &&
 
 ## Clone the WallWiz project
-git clone --depth 1 https://github.com/5hubham5ingh/WallWiz.git &&
+   git clone --depth 1 https://github.com/5hubham5ingh/WallWiz.git &&
 
 ## Build WallWiz then install it.
-cd WallWiz/src &&
-qjsc main.js -o WallWiz &&
-sudo cp WallWiz /usr/bin/ &&
-echo "WallWiz installation completed successfully."
+   cd WallWiz/src &&
+   qjsc -flto main.js -o WallWiz &&
+   sudo cp WallWiz /usr/bin/ &&
+   echo "WallWiz installation completed successfully."
--- src/utils.js
+++ src/utils.js
@@ -93,10 +93,31 @@ class Utils {
   }
 
   /**
-  * @param {string} dir - directory path
-  */
-  ensureDir(dir){
+   * @param {string} dir - directory path
+   */
+  ensureDir(dir) {
+    if (typeof dir !== "string") throw new TypeError("Invalid directory type.");
+    let directory;
+    switch (dir[0]) {
+      case "~":
+        directory = HOME_DIR.concat(dir.slice(1));
+        break;
+      case "/":
+        directory = dir;
+        break;
+      default: {
+        const path = OS.realpath(dir);
+        if (path[1] !== 0) throw new Error("Failed to read directory");
+        directory = path[0];
+      }
+    }
 
+    directory.split("/").forEach((dir, i, path) => {
+      if (!dir) return;
+      const currPath = path.filter((_, j) => j <= i).join("/");
+      const dirStat = OS.stat(currPath)[0];
+      if (!dirStat) OS.mkdir(currPath);
+    });
   }
 
   log(message) {
