225f880f9cb66dbf6af083dc016fdf133c438c4d
Shubham Singh <shubham.singh@mygstcafe.in>
2024-10-25 16:26:05 +0530

    Update: Changed the function export type

--- wallpaperDaemonHandlerScripts/hyprpaper@5hubham5ingh.js
+++ wallpaperDaemonHandlerScripts/hyprpaper@5hubham5ingh.js
@@ -1,10 +1,8 @@
-import { exec } from 'os'
 
-function setWallpaper(wallpaperPath, execAsync) {
-  exec(["hyprctl", "-q", "hyprpaper unload all"]);
-  exec(["hyprctl", "-q", `hyprpaper preload ${wallpaperPath}`]);
-  exec(["hyprctl", "-q", `hyprpaper wallpaper eDP-1,${wallpaperPath}`]);
+export function setWallpaper(wallpaperPath) {
+  OS.exec(["hyprctl", "-q", "hyprpaper unload all"]);
+  OS.exec(["hyprctl", "-q", `hyprpaper preload ${wallpaperPath}`]);
+  OS.exec(["hyprctl", "-q", `hyprpaper wallpaper eDP-1,${wallpaperPath}`]);
 }
 
 
-export default setWallpaper;
