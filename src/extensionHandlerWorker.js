c8aa2edd14602b7cd69ab92946f628e75194f765
Shubham Singh <shubham.singh@mygstcafe.in>
2024-12-12 17:08:24 +0530

    Improve: Worker thread to include theme config catching

--- src/extensionHandlerWorker.js
+++ src/extensionHandlerWorker.js
@@ -1,5 +1,6 @@
 import * as _ from "./globalConstants.js";
 import Color from "./Color/color.js";
+import utils from "./utils.js";
 
 globalThis.Color = Color;
 
@@ -54,6 +55,53 @@ const startWork = async (data) => {
     }
 
     parent.postMessage({ type: "success", data: results });
+  }, "startWork");
+};
+
+// TODO: impliment config caching here instead of delegating it to parent thread
+const startWork2 = async (data) => {
+  await catchAsyncError(async () => {
+    const { scriptPath, scriptMethods, args } = data;
+    const exports = await import(scriptPath);
+    for (const [functionName, cacheDir] of Object.entries(scriptMethods)) {
+      const cb = exports?.[functionName];
+      if (!cb) {
+        parent.postMessage({
+          type: "systemError",
+          data: (
+            "Error in " + scriptPath + ";" +
+            "No function named " + functionName + " found."
+          ),
+        });
+        break;
+      }
+      try {
+        const result = await cb(...args);
+        utils.writeFile(result, cacheDir);
+      } catch (status) {
+        if (status === EXIT) continue;
+
+        // TODO: Send the error in system notification
+        status instanceof SystemError
+          ? parent.postMessage({
+            type: "systemError",
+            data: [
+              status.name,
+              status.description,
+              JSON.stringify(status.body ?? ""),
+            ],
+          })
+          : parent.postMessage({
+            type: "error",
+            data: [
+              scriptPath,
+              JSON.stringify(status),
+            ],
+          });
+      }
+    }
+
+    parent.postMessage({ type: "success" });
   }, "startWork");
 };
 
